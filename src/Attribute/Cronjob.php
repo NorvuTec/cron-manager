048a1e871c810cadce1e6a5a7456eb643a874e14
Christian Linde <info@christian-linde.de>
2023-08-26 16:36:39 +0200

    Update Cronjob Attribute data

--- src/Attribute/Cronjob.php
+++ src/Attribute/Cronjob.php
@@ -14,12 +14,29 @@ class Cronjob {
 
     public function __construct(
         private string $tag,
-        private string $name
+        private string $name,
+        private string $cronExpression,
+        private ?array $commandArgs = null
     ) {}
+
+    public function getTag(): string
+    {
+        return $this->tag;
+    }
 
     public function getName(): string
     {
         return $this->name;
+    }
+
+    public function getCronExpression(): string
+    {
+        return $this->cronExpression;
+    }
+
+    public function getCommandArgs(): ?array
+    {
+        return $this->commandArgs;
     }
 
 }
