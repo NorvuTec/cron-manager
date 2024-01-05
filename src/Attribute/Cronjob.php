8435cd76b13e2883fb34a8ec46b940f5071a8ded
Christian Linde <info@christian-linde.de>
2024-01-05 06:18:25 +0100

    fix duplicate check to class check

--- src/Attribute/Cronjob.php
+++ src/Attribute/Cronjob.php
@@ -32,13 +32,13 @@ class Cronjob {
         string                  $cronExpression,
         private readonly ?array $commandArgs = null
     ) {
-        if(array_key_exists($this->tag, self::$knownTags) && self::$knownTags[$this->tag] != $this) {
+        if(array_key_exists($this->tag, self::$knownTags) && self::$knownTags[$this->tag] != get_class($this)) {
             throw new DuplicateCronjobTagException($this->tag);
         }
         if(strlen($this->tag) > 50) {
             throw new InvalidCronjobTagException($this->tag);
         }
-        self::$knownTags[$this->tag] = $this;
+        self::$knownTags[$this->tag] = get_class($this);
         try {
             $this->cronSchedule = new CronExpression($cronExpression);
         } catch (InvalidArgumentException $e) {
