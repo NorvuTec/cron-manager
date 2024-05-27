b417b825d928bb3fbae3022a003309cfaac93e83
Christian Linde <info@christian-linde.de>
2024-05-27 09:44:33 +0200

    update default response types

--- src/DependencyInjection/NorvutecCronManagerCompilerPass.php
+++ src/DependencyInjection/NorvutecCronManagerCompilerPass.php
@@ -9,8 +9,7 @@ use Symfony\Component\DependencyInjection\Reference;
 
 class NorvutecCronManagerCompilerPass implements CompilerPassInterface {
 
-    public function process(ContainerBuilder $container)
-    {
+    public function process(ContainerBuilder $container) : void {
         if(!$container->has(CronManagerService::class)) {
             return;
         }
